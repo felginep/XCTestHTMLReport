caf7db7947fee1cbd5807ee024fa5adc12a43ec0
Julien Rollet <julien.rollet@fabernovel.com>
2019-10-22 11:15:14 +0200

    Bump sha

--- xchtmlreport.rb
+++ xchtmlreport.rb
@@ -2,7 +2,7 @@ class Xchtmlreport < Formula
   desc "XCTestHTMLReport: Xcode-like HTML report for Unit and UI Tests"
   homepage "https://github.com/applidium/XCTestHTMLReport"
   url "https://github.com/applidium/XCTestHTMLReport/archive/2.0.1.tar.gz"
-  sha256 "99ce892200ac116ee551ab609045983070165b706cac08391f294c979ceab448"
+  sha256 "59ef4f1c9c876e8bbf0051b324b8de79cbf8dc134e8f16a21ee4ade059a203a4"
   head "https://github.com/applidium/XCTestHTMLReport.git", :branch => "develop_ad"
 
   def install
